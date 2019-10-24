1c4b6882b548325a77ce0979f68574436ffc016e
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2019-10-24 11:34:19 +0200

    Add files via upload

---
+++ kusanovicLenardVjezbaPetlje2/kusanovicLenardVjezbaPetlje2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardVjezbaPetlje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string unosTeksta;
+
+            Console.WriteLine("Unesi tekst: ");
+
+            unosTeksta = Console.ReadLine();
+
+            for (int i = unosTeksta.Length; i>0; i--)
+            {
+                Console.WriteLine(unosTeksta.Substring(0, i));
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
